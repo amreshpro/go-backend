0a3d0623b4684f6f07ff4e2115f2c9a51e3ba2ea
Amresh Maurya <amresh.terminal@gmail.com>
2025-04-22 16:23:23 +0530

    serve static folder

--- 2-static-file-serve/internal/handler/handler.go
+++ 2-static-file-serve/internal/handler/handler.go
@@ -6,10 +6,6 @@ import (
 
 
 
-func HTMLFileSender(w http.ResponseWriter, r *http.Request,filepath string)  {
-	http.ServeFile(w,r,filepath)
-}
-
 func HealthCheck(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("ok"))
 }
--- 2-static-file-serve/router/router.go
+++ 2-static-file-serve/router/router.go
@@ -2,15 +2,15 @@ package router
 
 import (
 	"net/http"
+
 	"github.com/amreshpro/go-backend/2-static-file-serve/internal/handler"
 )
 
-
-func AppRouter() http.Handler{
+func AppRouter() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-		handler.HTMLFileSender(w,r,"./web/index.html")
-	})
-	mux.HandleFunc("/health",handler.HealthCheck)
+	// Serve static files from ./web directory
+	fileServer := http.FileServer(http.Dir("./web"))
+	mux.Handle("/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/health", handler.HealthCheck)
 	return mux
 }
